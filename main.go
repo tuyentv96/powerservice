8fd30fb5ca89f3aba8864aef95f304e45f5f51c5
tuyentv <vat1906@gmail.com>
2017-06-03 11:06:43 +0700

    update

--- main.go
+++ main.go
@@ -3,31 +3,64 @@ package main
 import (
 	"gopkg.in/gin-gonic/gin.v1"
 	api "power/service"
+	"github.com/dgrijalva/jwt-go"
+
 )
+
+func respondWithError(code int, message string,c *gin.Context) {
+	resp := map[string]string{"error": message}
+
+	c.JSON(code, resp)
+	c.Abort()
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		jwt_string:= c.Request.Header.Get("auth")
+
+		token, err := jwt.Parse(jwt_string, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret123"), nil
+		})
+
+
+		if err == nil && token.Valid {
+			c.Next()
+			return
+
+		}
+		respondWithError(401, "Permission denied", c)
+		return
+
+	}
+}
 
 
 func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
+	r.Use(TokenAuthMiddleware())
+
 	r.GET("/getDevice",api.GetDevicePower)
-	r.GET("/getPowerDeviceLimit",api.GetDevicePowerLimit)
-	r.GET("/getPowerDeviceByTime",api.GetDevicePowerByTime)
+	r.GET("/getDeviceLimit",api.GetDevicePowerLimit)
+	r.GET("/getDeviceByTime",api.GetDevicePowerByTime)
 
-	r.GET("/getDevicePowerInMonth",api.GetDevicePowerInMonth)
-	r.GET("/getDevicePowerInYear",api.GetDevicePowerInYear)
+	r.GET("/getDeviceInMonth",api.GetDevicePowerInMonth)
+	r.GET("/getDeviceInYear",api.GetDevicePowerInYear)
 
-	r.GET("/getHomePowerInYear",api.GetHomePowerInYear)
-	r.GET("/getHomePowerInMonth",api.GetHomePowerInMonth)
+	r.GET("/getRankInMonth",api.GetRankingDevicePowerInMonth)
+	r.GET("/getRankInYear",api.GetRankingDevicePowerInYear)
+	r.GET("/getRankAll",api.GetRankingDevicePowerAll)
 
-	r.GET("/getRankingPowerInMonth",api.GetRankingDevicePowerInMonth)
-	r.GET("/getRankingPowerInYear",api.GetRankingDevicePowerInYear)
-	r.GET("/getRankingPowerAll",api.GetRankingDevicePowerAll)
+	r.GET("/getHomeInYear",api.GetHomePowerInYear)
+	r.GET("/getHomeInMonth",api.GetHomePowerInMonth)
+
+
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run("127.0.0.1:8080") // listen and serve on 0.0.0.0:8080
+	r.Run("127.0.0.1:8081") // listen and serve on 0.0.0.0:8080
 }
